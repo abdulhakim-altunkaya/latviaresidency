65a9608640c338737415ad024515d00052abdd0a
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-01-10 00:35:27 +0300

    company setup part 4 is finished

--- client/src/components/BottomCompany.js
+++ client/src/components/BottomCompany.js
@@ -11,6 +11,7 @@ function BottomCompany() {
 
           <h3>Şirket Türleri</h3>
           <div>
+            <p>Letonya'daki belli başlı şirket türleri aşağıda karşılaştırmalı olarak sunulmaktadır.</p>
             <table className='companyTypesTable'>
               <tr>
                 <th></th>
@@ -20,45 +21,67 @@ function BottomCompany() {
                 <th>Şahıs Şirketi</th>
               </tr>
               <tr>
-                <td>Letonca isim</td>
+                <td><strong>Letonca isim</strong></td>
                 <td>Sabiedrība ar ierobežotu atbildību SIA</td>
                 <td>Mazkapitāla SIA</td>
                 <td>Akciju sabiedrība AS</td>
                 <td>Individuālais komersants IK</td>
               </tr>
               <tr>
-                <td>Minimum Sermaye Tutaru</td>
+                <td><strong>Minimum Sermaye Tutaru</strong></td>
                 <td>2800 Euro</td>
                 <td>1-2799 Euro</td>
                 <td>25000</td>
                 <td>-</td>
               </tr>
               <tr>
-                <td>Oturum Hakkı</td>
+                <td><strong>Oturum Hakkı</strong></td>
                 <td>Verir</td>
                 <td>Vermez</td>
                 <td>Verir</td>
                 <td>Vermez</td>
               </tr>
               <tr>
-                <td>Kurucu Sayısı</td>
+                <td><strong>Kurucu Sayısı</strong></td>
                 <td>Birden çok</td>
                 <td>1-5 arası</td>
                 <td>Birden çok</td>
                 <td>1</td>
               </tr>
               <tr>
-                <td>Kurucu Tipi</td>
+                <td><strong>Kurucu Tipi</strong></td>
                 <td>Gerçek ve/veya Tüzel</td>
                 <td>Sadece Gerçek</td>
                 <td>Gerçek ve/veya Tüzel</td>
                 <td>Sadece Gerçek</td>
               </tr>
+              <tr>
+                <td><strong>Yönetim Kurulu (YK) Üyeleri</strong></td>
+                <td>Kurucular en az bir YK üyesi atar.</td>
+                <td>Bütün Kurucular aynı zamanda YK üyesidir</td>
+                <td>Kurucular ve YK üyeleri farklı olabilir</td>
+                <td>Yönetim kurulu yoktur.</td>
+              </tr>
+              <tr>
+                <td><strong>Kurumlar Vergisi (Yıllık karın %20-%25'i)</strong></td>
+                <td>Öder</td>
+                <td>Öder</td>
+                <td>Öder</td>
+                <td>Ödemez</td>
+              </tr>
+              <tr>
+                <td><strong>KDV</strong></td>
+                <td>Öder</td>
+                <td>Öder</td>
+                <td>Öder</td>
+                <td>Öder</td>
+              </tr>
             </table>
           </div>
 
-          <h3>Limited Şirket ("Sabiedrība ar ierobežotu atbildību (SIA)")</h3>
-          <div>
+          
+          <div className='bottomCompanyText'>
+          <h3>Limited Şirket (Sabiedrība ar ierobežotu atbildību  SIA)</h3>
             <p>Limited Şirket Letonya'da popüler ve kurulması kolay bir şirket türüdür.
               Limited Şirketler kendi içinde ikiye ayrılır: Standart sermayeli (minimum sermaye 2800 Euro) ve 
               Küçük sermayeli (1 ila 2799 Euro arası).
@@ -70,7 +93,8 @@ function BottomCompany() {
             </p>
             <p>Limited Şirket kurucuları şirket kurulduktan sonra şirket hissedarı olurlar. Şirket kurucusu/hissedarı olmak 
               için Letonya vatandaşı olma veya Letonya'da ikamet etme şartı yoktur. Limited Şirkete verilecek adın Letonya'da
-              başka bir şirket adıyla veya marka ismiyle aynı olmaması gerekmektedir.
+              başka bir şirket adıyla veya marka ismiyle aynı olmaması gerekmektedir. Kurucular şirketin yönetim kuruluna en 
+              az bir kişiyi atarlar. Yönetim kurulu üyesi olmak için Letonya vatandaşı olma veya Letonya'da yaşama şartı yoktur.
             </p>
             <p>Limited Şirket Ticaret Siciline kaydedildiği anda vergi mükellefi de olur. Limited Şirketini için KDV numarasını
               kayıt işlemleri yapılırken alınabilir.
@@ -78,12 +102,12 @@ function BottomCompany() {
             <p>Limited şirket kurulumu için Letonya'yı ziyaret edebilir, süreci yakından takip edebilir ve ileride çok
               işinize yarayacak elektronik imzanızı da alabilirsiniz.
             </p>
-            <p><strong>Süreç:</strong></p>
+            
             <div className='processTable'>
               <div className='processTableRows'>
                 <span className='processTableBox'>Müşterimizin bilgi ve belgeleri alınır.</span>
                 <img src='/svg_arrow3.svg' className='processTableArrows' alt='arrow icon'/>
-                <span className='processTableBox'>Müşterimiz şirketimize vekaletname verir.</span>
+                <span className='processTableBox'>Şirket belgeleri hazırlanır, müşteri tarafından imzalanır.</span>
                 <img src='/svg_arrow3.svg' className='processTableArrows' alt='arrow icon'/>
                 <span className='processTableBox'>Şirket adresi belirlenir, başvuru dosyası hazırlanır.
                 </span>
@@ -101,17 +125,62 @@ function BottomCompany() {
               <div className='processTableRows'>
                 <span className='processTableBox'>Opsiyonel: Müşterimiz Letonya'ya gelip e-imza başvurusu yapabilir.</span>
                 <img src='/svg_arrow3.svg' className='processTableArrows' alt='arrow icon'/>
-                <span className='processTableBox'>Opsiyonel: Müşterimiz Letonya'ya gelip asıl banka hesabı 
-                  başvurusu yapabilir.</span>
+                <span className='processTableBox'>Opsiyonel: Müşterimiz Letonya'ya gelip geçici hesabı normal banka hesabına 
+                  dönüştürebilir.</span>
                 <img src='/svg_arrow3.svg' className='processTableArrows' alt='arrow icon'/>
                 <span className='processTableBox'>Müşterimize kaşeleri ve şirket belgeleri teslim edilir.</span>
               </div>
             </div>
+            <br/>
+            <ol>
+              <strong>Gerekli Belge ve Bilgiler:</strong>
+              <li>Kurucuların geçerli pasaport bilgileri (Pasaportun önsayfasının taranmış hali)</li>
+              <li>Kurucuların ikamet adresi bilgisi (E-devlet yerleşim yeri belgesi)</li>
+              <li>Kurucuların temsilcilerinin bilgileri (Temsicileriniz olarak kendi
+                kişisel bilgilerimizi Şirket Sicil Kurumu'na sunuyoruz)</li>
+              <li>Yönetim Kurulu üyelerinin geçerli pasaport ve ikamet adresi bilgileri</li>
+              <li>Şirketin sözleşmesi (Biz hazırlayacağız)</li>
+              <li>Şirketin ana sermayesinin yatırıldığına dair banka belgesi (Biz hazırlayacağız)</li>
+              <li>Şirketin adres bilgisi (Biz hazırlayacağız)</li>
+              <li>Kurucu ve YK üyelerinin şart olmasa da Türkiye'den alacakları apostilli ve Letonca'ya tercümeli adli sicil belgesi
+                veya Riga Büyükelçiliği Konsolosluğundan alacağınız apostilsiz ve Letonca adli sicil belgesi
+              </li>
+              <li>Kuruculardan biri tüzel kişilik ise (Türkiye'de kayıtlı bir şirket gibi), 
+                <br/> - Şirket kuruluş belgesi (Ticaret Sicil Gazetesi)
+                <br/> - Şirket adres bilgisi (Ticaret Kütük Kaydı)
+                <br/> - Şirket iyi hal belgesi (Ticaret Sicil Memurlukları)
+                <br/> - Şirket kuruluş sözleşmesi
+                <br/> - İmza sirküleri
+                <br/> - Faaliyet belgesi
+              </li>
+            </ol>
+            <div>
+              <br/>
+              <ul>
+              Daha detaylı bilgi için:
+                <li>
+                  <a href='https://www.ur.gov.lv/en/register/company-or-merchant/limited-liability-company-sia/founding/registration-with-the-enterprise-register/'>
+                    Şirket Sicil Kurumu (Latvijas Republikas Uzņēmumu reģistrs)
+                  </a>
+                </li>
 
-            
-              <ol>
-                <li></li>
-              </ol>
+                <li>
+                  <a href='https://www.vid.gov.lv/en'>
+                    Devlet Gelir İdaresi (Valsts ieņēmumu dienests VID)
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.liaa.gov.lv/en/invest-latvia'>
+                    Letonya Yatırım ve Kalkınma Ajansı (Latvijas Investīciju un attīstības aģentūra LIAA)
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.njordlaw.com/lv'>
+                    Njord Letonya Kurumsal Hizmetler Firması
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div> 
           
         </div>
